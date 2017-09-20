4a0b8818c4f24ca1913bdd004b10ca0740603baa
Nicilo <nick.ilog@yahoo.com>
2017-09-19 17:06:09 -0700

    teacher class

---
+++ Teacher.java
@@ -0,0 +1,43 @@
+public class Teacher {
+   public String getTLastName() {
+      return tLastName;
+   }
+
+   public void setTLastName(String tLastName) {
+      this.tLastName = tLastName;
+   }
+
+   public String getTFirstName() {
+      return tFirstName;
+   }
+
+   public void setTFirstName(String tFirstName) {
+      this.tFirstName = tFirstName;
+   }
+
+   public int getClassroom() {
+      return classroom;
+   }
+
+   public void setClassroom(int classroom) {
+      this.classroom = classroom
+   }
+
+   public void print() {
+      System.out.println("Last Name is - " + tLastName);
+      System.out.println("First Name is - " + tFirstName);
+      System.out.println("Classroom is - " + classsroom);
+      System.out.println("----------------------------------");
+   }
+
+   public Teacher(String tLastName, String tFirstName, int classroom) {
+      super();
+      this.tLastName = tLastName;
+      this.tFirstName = tFirstName;
+      this.classroom = classroom;
+   }
+
+   String tLastName;
+   String tFirstName;
+   int classroom;
+}
